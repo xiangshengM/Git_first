099dd0c97ba44ed42bd5d7c99d16cdc2d666f7aa
xiangshengM <1054969234@qq.com>
2021-03-01 13:16:48 +0800

    完成了首页的功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,4 +5,28 @@ $.ajaxPrefilter(function (options) {
     //在发起真正的ajax之前，同意拼接请求的路径
     options.url = 'http://ajax.frontend.itheima.net'
         + options.url
+    // options.url = 'http://api-breakingnews-web.itheima.net/'
+    // + options.url
+
+
+    //统一尾有权限的接口 设置headers请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    //全局统一挂载，complete回调函数
+    options.complete = function (res) {
+        // console.log('执行了 complete回调：');
+        // console.log(res);
+        //在complete回调函数中，可以使用res.responseJSON拿到服务器相应回来的数据
+        if (res.responseJSON.sratus === 1 && res.responseJSON.message === '身份验证失败') {
+            //1.强行清空 token
+            localStorage.removeItem('token')
+            //2.强行跳转到登录页面
+            localStorage.href = '/login.html'
+        }
+    }
 })
+
